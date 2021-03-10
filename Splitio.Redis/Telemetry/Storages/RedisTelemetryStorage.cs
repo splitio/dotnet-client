5092bd0332a6baed35f1db88b78f4f2a06ba38eb
sanzmauro <mauro.sanz@split.io>
2021-03-10 09:57:16 -0300

    Redis telemetry storage implementation and tests

--- Splitio.Redis/Telemetry/Storages/RedisTelemetryStorage.cs
+++ Splitio.Redis/Telemetry/Storages/RedisTelemetryStorage.cs
@@ -1,28 +1,53 @@
-﻿using System.Collections.Generic;
+﻿using Splitio.Redis.Services.Cache.Interfaces;
 using Splitio.Telemetry.Domain;
 using Splitio.Telemetry.Domain.Enums;
 using Splitio.Telemetry.Storages;
+using System.Collections.Generic;
 
 namespace Splitio.Redis.Telemetry.Storages
 {
     public class RedisTelemetryStorage : ITelemetryStorage
     {
-        public void RecordException(MethodEnum method)
+        private const string TelemetryLatencyKey = "{prefix}.SPLITIO.telemetry.latencies.{metadata}.{method}.{bucket}";
+        private const string TelemetryExceptionKey = "{prefix}.SPLITIO.telemetry.exceptions.{metadata}.{method}";
+
+        private readonly IRedisAdapter _redisAdapter;
+        private readonly string _userPrefix;
+        private readonly string _sdkVersion;
+        private readonly string _machineIp;
+        private readonly string _machineName;
+
+        public RedisTelemetryStorage(IRedisAdapter redisAdapter,
+            string userPrefix,
+            string sdkVersion,
+            string machineIp,
+            string machineName)
         {
-            throw new System.NotImplementedException("Not implemented for redis.");
+            _redisAdapter = redisAdapter;
+            _userPrefix = userPrefix;
+            _sdkVersion = sdkVersion;
+            _machineIp = machineIp;
+            _machineName = machineName;
         }
 
+        #region Public Methods
+        public void RecordException(MethodEnum method)
+        {
+            _redisAdapter.HashIncrement(BuildKeyException(method.ToString()), 1);
+        }
+
+        public void RecordLatency(MethodEnum method, int bucket)
+        {
+            _redisAdapter.HashIncrement(BuildKeyLatency(method.ToString(), bucket), 1);
+        }
+        #endregion
+
+        #region Not Implemented Methods
         public void RecordSyncLatency(HttpLatenciesEnum path, int bucket)
         {
             throw new System.NotImplementedException("Not implemented for redis.");
         }
 
-        public void RecordLatency(MethodEnum method, int bucket)
-        {
-            throw new System.NotImplementedException("Not implemented for redis.");
-        }
-
-        #region Not Implemented Methods
         public void AddTag(string tag)
         {
             throw new System.NotImplementedException("Not implemented for redis.");
@@ -146,6 +171,30 @@ namespace Splitio.Redis.Telemetry.Storages
         public void RecordTokenRefreshes()
         {
             throw new System.NotImplementedException("Not implemented for redis.");
+        }
+        #endregion
+
+        #region Private Methods
+        private string FormatMetadata()
+        {
+            return $"[{_sdkVersion}.{_machineIp}.{_machineName}]";
+        }
+
+        private string BuildKeyLatency(string method, int bucket)
+        {
+            return TelemetryLatencyKey
+                .Replace("{prefix}.", string.IsNullOrEmpty(_userPrefix) ? string.Empty : $"{_userPrefix}.")
+                .Replace("{metadata}", FormatMetadata())
+                .Replace("{method}", method)
+                .Replace("{bucket}", bucket.ToString());
+        }
+
+        private string BuildKeyException(string method)
+        {
+            return TelemetryExceptionKey
+                .Replace("{prefix}.", string.IsNullOrEmpty(_userPrefix) ? string.Empty : $"{_userPrefix}.")
+                .Replace("{metadata}", FormatMetadata())
+                .Replace("{method}", method);
         }
         #endregion
     }
