96247bc44ae7ce17fdb802d7f82af3cc2c2810a9
Mauro <mauro.sanz@split.io>
2022-02-17 16:39:55 -0300

    Custom BloomFilter implementation

---
+++ Splitio-tests/Unit Tests/Cache/Filter/BloomFilterTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Splitio.Services.Cache.Filter;
+using System;
+using System.Collections.Generic;
+
+namespace Splitio_Tests.Unit_Tests.Cache.Filter
+{
+    [TestClass]
+    public class BloomFilterTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            // Arrange.
+            var expectedKeys = new List<string>
+            {
+                $"key-{Guid.NewGuid()}",
+                $"key-{Guid.NewGuid()}",
+                $"key-{Guid.NewGuid()}",
+                $"key-{Guid.NewGuid()}",
+                $"key-{Guid.NewGuid()}",
+                $"key-{Guid.NewGuid()}",
+                $"key-{Guid.NewGuid()}",
+            };
+            var bf = new BloomFilter(expectedElements: 1000, errorRate: 0.01);
+
+            // Act.
+            for (int i = 0; i < 500; i++)
+            {
+                Assert.IsTrue(bf.Add($"key-{Guid.NewGuid()}"));
+            }
+
+            foreach (var item in expectedKeys)
+            {
+                Assert.IsTrue(bf.Add(item));
+            }
+
+            // Assert.
+            foreach (var item in expectedKeys)
+            {
+                Assert.IsFalse(bf.Add(item));
+            }
+
+            foreach (var item in expectedKeys)
+            {
+                Assert.IsTrue(bf.Contains(item));
+            }
+
+            foreach (var item in expectedKeys)
+            {
+                Assert.IsFalse(bf.Contains($"{item}-fail"));
+            }
+        }
+    }
+}
---
+++ src/Splitio/Services/Cache/Filter/IBloomFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace Splitio.Services.Cache.Filter
+{
+    public interface IBloomFilter
+    {
+        bool Add(string data);
+        bool Contains(string data);
+        void Clear();
+    }
+}
