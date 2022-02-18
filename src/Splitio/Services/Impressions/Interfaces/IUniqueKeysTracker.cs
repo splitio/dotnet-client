874a6991cb15a812f37ba730e6bfebc5863d1d2b
Mauro <mauro.sanz@split.io>
2022-02-18 11:51:57 -0300

    undo Uniquekeystracker

--- src/Splitio/Services/Impressions/Interfaces/IUniqueKeysTracker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Splitio.Services.Impressions.Interfaces
-{
-    public interface IUniqueKeysTracker
-    {
-        void Start();
-        void Stop();
-        void Track(string key, string featureName);
-    }
-}
