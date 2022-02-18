34a23d635d9d5138b1e71ea33e1af448289cb643
Mauro <mauro.sanz@split.io>
2022-02-18 10:57:52 -0300

    UniqueKeys tracker implementation

---
+++ src/Splitio/Services/Impressions/Interfaces/IUniqueKeysTracker.cs
@@ -0,0 +1,9 @@
+﻿namespace Splitio.Services.Impressions.Interfaces
+{
+    public interface IUniqueKeysTracker
+    {
+        void Start();
+        void Stop();
+        void Track(string key, string featureName);
+    }
+}
