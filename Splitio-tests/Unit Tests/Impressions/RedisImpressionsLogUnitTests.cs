a37ca92dc2be6e81ef393c5ed9ccdfc46ce23285
Mauro Sanz <mauro.sanz@split.io>
2022-10-04 18:13:38 -0300

    fixed uniquekeys in redis

--- Splitio-tests/Unit Tests/Impressions/RedisImpressionsLogUnitTests.cs
+++ Splitio-tests/Unit Tests/Impressions/RedisImpressionsLogUnitTests.cs
@@ -6,7 +6,6 @@ using Splitio.Services.Shared.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 
 namespace Splitio_Tests.Unit_Tests.Impressions
 {
