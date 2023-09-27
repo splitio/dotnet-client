991fa9e66d06f32be781ba5709954f7b8bba9e2b
Mauro Sanz <mauro.sanz@split.io>
2023-09-27 16:06:59 -0300

    removed ISplitClientSync

--- src/Splitio/Services/Client/Interfaces/ISplitClient.cs
+++ src/Splitio/Services/Client/Interfaces/ISplitClient.cs
@@ -1,7 +1,20 @@
-﻿namespace Splitio.Services.Client.Interfaces
+﻿using Splitio.Domain;
+using System.Collections.Generic;
+
+namespace Splitio.Services.Client.Interfaces
 {
-    public interface ISplitClient : ISplitClientSync, ISplitClientAsync
+    public interface ISplitClient : ISplitClientAsync
     {
+        string GetTreatment(string key, string feature, Dictionary<string, object> attributes = null);
+        string GetTreatment(Key key, string feature, Dictionary<string, object> attributes = null);
+        SplitResult GetTreatmentWithConfig(string key, string feature, Dictionary<string, object> attributes = null);
+        SplitResult GetTreatmentWithConfig(Key key, string feature, Dictionary<string, object> attributes = null);
+        Dictionary<string, string> GetTreatments(string key, List<string> features, Dictionary<string, object> attributes = null);
+        Dictionary<string, string> GetTreatments(Key key, List<string> features, Dictionary<string, object> attributes = null);
+        Dictionary<string, SplitResult> GetTreatmentsWithConfig(string key, List<string> features, Dictionary<string, object> attributes = null);
+        Dictionary<string, SplitResult> GetTreatmentsWithConfig(Key key, List<string> features, Dictionary<string, object> attributes = null);
+        bool Track(string key, string trafficType, string eventType, double? value = null, Dictionary<string, object> properties = null);
+        void Destroy();
         ISplitManager GetSplitManager();
         bool IsDestroyed();
         void BlockUntilReady(int blockMilisecondsUntilReady);
