40d1e4109827a9df467b925c1e9f9cdb28ec0f47
Mauro Sanz <mauro.sanz@split.io>
2023-09-27 10:32:54 -0300

    updated impression listener

--- Splitio.Integration-tests/Resources/IntegrationTestsImpressionListener.cs
+++ Splitio.Integration-tests/Resources/IntegrationTestsImpressionListener.cs
@@ -1,21 +1,21 @@
 ﻿using Splitio.Domain;
 using Splitio.Services.Impressions.Interfaces;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace Splitio.Integration_tests.Resources
 {
     public class IntegrationTestsImpressionListener : IImpressionListener
     {
-        private readonly ConcurrentDictionary<string, KeyImpression> _queue;
+        private readonly Dictionary<string, KeyImpression> _queue;
 
         public IntegrationTestsImpressionListener(int size)
         {
-            _queue = new ConcurrentDictionary<string, KeyImpression>();
+            _queue = new Dictionary<string, KeyImpression>();
         }
 
         public void Log(KeyImpression impression)
         {
-            _queue.TryAdd($"{impression.feature}::{impression.keyName}", impression);
+            _queue.Add($"{impression.feature}::{impression.keyName}", impression);
         }
 
         public KeyImpression Get(string feature, string keyName)
