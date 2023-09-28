e4b3ae9989d52484dae30d369a2a7369949b9f41
Mauro Sanz <mauro.sanz@split.io>
2023-09-28 16:51:48 -0300

    removed delay

--- Splitio.Integration-tests/BaseIntegrationTests.cs
+++ Splitio.Integration-tests/BaseIntegrationTests.cs
@@ -823,10 +823,9 @@ namespace Splitio.Integration_tests
         protected abstract ConfigurationOptions GetConfigurationOptions(int? eventsPushRate = null, int? eventsQueueSize = null, int? featuresRefreshRate = null, bool? ipAddressesEnabled = null, IImpressionListener impressionListener = null);
         protected abstract Task AssertSentImpressionsAsync(int sentImpressionsCount, params KeyImpression[] expectedImpressions);
         protected abstract Task AssertSentEventsAsync(List<EventBackend> eventsExcpected, int sleepTime = 15000, int? eventsCount = null, bool validateEvents = true);
-
         protected virtual async Task DelayAsync()
         {
-            await Task.Delay(1000);
+            await Task.Delay(0);
         }
         #endregion
     }
