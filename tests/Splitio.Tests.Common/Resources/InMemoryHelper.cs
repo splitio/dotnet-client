bd2d9af88d296d01635a3a6e36877c264673aba0
Mauro Sanz <mauro.sanz@split.io>
2023-11-24 10:50:25 -0300

    adding delay

--- tests/Splitio.Tests.Common/Resources/InMemoryHelper.cs
+++ tests/Splitio.Tests.Common/Resources/InMemoryHelper.cs
@@ -58,7 +58,7 @@ namespace Splitio.Tests.Common.Resources
 
             var sentImpressions = new List<KeyImpressionBackend>();
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 10; i++)
             {
                 sentImpressions = GetImpressionsSentBackend(httpClientMock);
 
@@ -107,7 +107,7 @@ namespace Splitio.Tests.Common.Resources
         {
             var sentEvents = new List<EventBackend>();
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 10; i++)
             {
                 sentEvents = GetEventsSentBackend(httpClientMock);
 
