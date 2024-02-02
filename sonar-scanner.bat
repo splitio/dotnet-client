09259026c0134307e9bb7491547b1235e6aae095
Mauro Sanz <mauro.sanz@split.io>
2024-02-02 13:53:33 -0300

    update sonar-scanner

--- sonar-scanner.bat
+++ sonar-scanner.bat
@@ -12,7 +12,9 @@ SonarScanner.MSBuild.exe begin ^
   /d:sonar.host.url="https://sonarqube.split-internal.com" ^
   /d:sonar.login="%SONAR_LOGIN%" ^
   /d:sonar.ws.timeout="300" ^
-  /d:sonar.links.ci="https://travis-ci.com/splitio/dotnet-client" ^
+  /d:sonar.tests=. ^
+  /d:sonar.cs.coverage.reportsPaths="**\TestResults\*\*.coverage" ^
+  /d:sonar.links.ci="https://ci.appveyor.com/project/SplitDevOps/dotnet-client" ^
   /d:sonar.links.scm="https://github.com/splitio/dotnet-client" ^
   %*
 EXIT /B 0
