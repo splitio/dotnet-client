ccebbad03007cc9a63cf74ca6b09f6b611499d9e
Mauro Sanz <mauro.sanz@split.io>
2024-04-05 17:57:09 -0300

    InListSemverMatcher implementation

---
+++ src/Splitio/Services/Parsing/Classes/InListSemverMatcher.cs
@@ -0,0 +1,25 @@
+﻿using Splitio.Services.Evaluator;
+using System.Collections.Generic;
+
+namespace Splitio.Services.Parsing.Classes
+{
+    public class InListSemverMatcher : BaseMatcher
+    {
+        private readonly List<string> _list;
+
+        public InListSemverMatcher(List<string> list)
+        {
+            _list = list;
+        }
+
+        public override bool Match(string key, Dictionary<string, object> attributes = null, IEvaluator evaluator = null)
+        {
+            if (key == null || _list == null)
+            {
+                return false;
+            }
+
+            return _list.Contains(key);
+        }
+    }
+}
