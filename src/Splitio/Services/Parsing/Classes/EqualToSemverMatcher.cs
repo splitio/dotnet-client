18b68a63785a71dafe02e864db9b8fbd36ae30cc
Mauro Sanz <mauro.sanz@split.io>
2024-04-05 11:58:21 -0300

    EqualToSemver implementation

---
+++ src/Splitio/Services/Parsing/Classes/EqualToSemverMatcher.cs
@@ -0,0 +1,25 @@
+﻿using Splitio.Services.Evaluator;
+using System.Collections.Generic;
+
+namespace Splitio.Services.Parsing.Classes
+{
+    public class EqualToSemverMatcher : BaseMatcher
+    {
+        private readonly string _target;
+
+        public EqualToSemverMatcher(string target)
+        {
+            _target = target;
+        }
+
+        public override bool Match(string key, Dictionary<string, object> attributes = null, IEvaluator evaluator = null)
+        {
+            if (key == null || _target == null)
+            {
+                return false;
+            }
+
+            return key == _target;
+        }
+    }
+}
