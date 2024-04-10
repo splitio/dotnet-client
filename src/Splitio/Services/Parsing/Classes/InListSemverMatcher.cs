e4ae0e29b6d6cc3adcebe1920d0d425a89a2f1df
Mauro Sanz <mauro.sanz@split.io>
2024-04-10 15:42:26 -0300

    polishing

--- src/Splitio/Services/Parsing/Classes/InListSemverMatcher.cs
+++ src/Splitio/Services/Parsing/Classes/InListSemverMatcher.cs
@@ -1,25 +1,48 @@
 ﻿using Splitio.Services.Evaluator;
+using Splitio.Services.SemverImp;
 using System.Collections.Generic;
 
 namespace Splitio.Services.Parsing.Classes
 {
     public class InListSemverMatcher : BaseMatcher
     {
-        private readonly List<string> _list;
+        private readonly List<Semver> _targetList = new List<Semver>();
 
         public InListSemverMatcher(List<string> list)
         {
-            _list = list;
+            list = list ?? new List<string>();
+
+            foreach (var item in list)
+            {
+                var semver = Semver.Build(item);
+
+                if (semver != null)
+                {
+                    _targetList.Add(semver);
+                }
+            }
         }
 
         public override bool Match(string key, Dictionary<string, object> attributes = null, IEvaluator evaluator = null)
         {
-            if (key == null || _list == null)
+            if (key == null || _targetList.Count == 0)
             {
                 return false;
             }
 
-            return _list.Contains(key);
+            var keySemver = Semver.Build(key);
+            if (keySemver == null)
+            {
+                return false;
+            }
+
+            foreach (var item in _targetList)
+            {
+                if (keySemver.Version.Equals(item.Version))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
