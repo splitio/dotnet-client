466c2a99fa6cf7a011503907abf3f4d98f58a82b
Mauro Antonio Sanz <mauro.sanz@split.io>
2025-02-25 11:34:24 -0300

    adding rbsParser tests

---
+++ tests/Splitio-tests/Unit Tests/Parsing/RuleBasedSegmentParserTest.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Splitio.Domain;
+using Splitio.Services.Cache.Interfaces;
+using Splitio.Services.Parsing;
+using Splitio.Services.SegmentFetcher.Interfaces;
+
+namespace Splitio_Tests.Unit_Tests.Parsing
+{
+    [TestClass]
+    public class RuleBasedSegmentParserTest
+    {
+        private Mock<ISegmentCacheConsumer> _segmentCacheMock;
+        private Mock<ISegmentFetcher> _segmentFetcherMock;
+        private RuleBasedSegmentParser _parser;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _segmentCacheMock = new Mock<ISegmentCacheConsumer>();
+            _segmentFetcherMock = new Mock<ISegmentFetcher>();
+            _parser = new RuleBasedSegmentParser(_segmentCacheMock.Object, _segmentFetcherMock.Object);
+        }
+
+        [TestMethod]
+        public void Parse_ValidDTO_ReturnsRuleBasedSegment()
+        {
+            // Arrange
+            var rbsDTO = new RuleBasedSegmentDTO
+            {
+                Status = "ACTIVE",
+                Name = "test-segment",
+                ChangeNumber = 123,
+                Excluded = new Excluded{
+                    Keys = new List<string> { "user1", "user2" }
+                },
+                Conditions = new List<ConditionDefinition>
+                {
+                    new ConditionDefinition
+                    {
+                        matcherGroup = new MatcherGroupDefinition
+                        {
+                            matchers = new List<MatcherDefinition>
+                            {
+                                new MatcherDefinition { matcherType = "EQUAL_TO", stringMatcherData = "test" }
+                            },
+                            combiner = "AND"
+                        }
+                    }
+                }
+            };
+
+            // Act
+            var result = _parser.Parse(rbsDTO);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("test-segment", result.Name);
+            Assert.AreEqual(123, result.ChangeNumber);
+            CollectionAssert.AreEqual(new List<string> { "user1", "user2" }, result.Excluded.Keys);
+            Assert.AreEqual(1, result.CombiningMatchers.Count);
+        }
+
+        [TestMethod]
+        public void Parse_InvalidStatus_ReturnsNull()
+        {
+            // Arrange
+            var rbsDTO = new RuleBasedSegmentDTO
+            {
+                Status = "ARCHIVE",
+                Name = "test-segment",
+                ChangeNumber = 123,
+                Excluded = new Excluded{
+                    Keys = new List<string> { "user1", "user2" }
+                },
+                Conditions = new List<ConditionDefinition>()
+            };
+
+            // Act
+            var result = _parser.Parse(rbsDTO);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Missing or empty matchers")]
+        public void Parse_MissingMatchers_ThrowsException()
+        {
+            // Arrange
+            var rbsDTO = new RuleBasedSegmentDTO
+            {
+                Status = "ACTIVE",
+                Name = "test-segment",
+                ChangeNumber = 123,
+                Excluded = new Excluded{
+                    Keys = new List<string> { "user1", "user2" }
+                },
+                Conditions = new List<ConditionDefinition>
+                {
+                    new ConditionDefinition
+                    {
+                        matcherGroup = new MatcherGroupDefinition
+                        {
+                            matchers = null,
+                            combiner = "AND"
+                        }
+                    }
+                }
+            };
+
+            // Act
+            _parser.Parse(rbsDTO);
+        }
+    }
+}
