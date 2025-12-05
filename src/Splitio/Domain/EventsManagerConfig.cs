cb26c84b27c535054921f5947630663f6b612efa
Bill Al <bilal.al-shahwany@harness.io>
2025-12-05 10:59:49 -0800

    added events manager config

---
+++ src/Splitio/Domain/EventsManagerConfig.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace Splitio.Domain
+{
+    public class EventsManagerConfig
+    {
+        public Dictionary<SdkEvent, HashSet<SdkInternalEvent>> RequireAll { get; private set; }
+        public Dictionary<SdkEvent, HashSet<SdkInternalEvent>> RequireAny { get; private set; }
+        public Dictionary<SdkEvent, HashSet<SdkInternalEvent>> Prerequisites { get; private set; }
+        public Dictionary<SdkEvent, HashSet<SdkInternalEvent>> SuppressedBy { get; private set; }
+        public Dictionary<SdkEvent, int> ExecutionLimits { get; private set; }
+
+        private EventsManagerConfig(
+            Dictionary<SdkEvent, HashSet<SdkInternalEvent>> requireAll,
+            Dictionary<SdkEvent, HashSet<SdkInternalEvent>> requireAny,
+            Dictionary<SdkEvent, HashSet<SdkInternalEvent>> prerequisites,
+            Dictionary<SdkEvent, HashSet<SdkInternalEvent>> suppressedBy,
+            Dictionary<SdkEvent, int> executionLimits)
+        { 
+            RequireAll = requireAll;
+            RequireAny = requireAny;
+            Prerequisites = prerequisites;
+            SuppressedBy = suppressedBy;
+            ExecutionLimits = executionLimits;
+        }
+
+        public static EventsManagerConfig BuildEventsManagerConfig() 
+        {
+            Dictionary<SdkEvent, HashSet<SdkInternalEvent>> requireAll = new Dictionary<SdkEvent, HashSet<SdkInternalEvent>>();
+            Dictionary<SdkEvent, HashSet<SdkInternalEvent>> prerequisites = new Dictionary<SdkEvent, HashSet<SdkInternalEvent>>();
+            Dictionary<SdkEvent, HashSet<SdkInternalEvent>> requireAny = new Dictionary<SdkEvent, HashSet<SdkInternalEvent>>
+            {
+                { SdkEvent.SdkUpdate, new HashSet<SdkInternalEvent> 
+                    {
+                            SdkInternalEvent.RuleBasedSegmentsUpdated,
+                            SdkInternalEvent.FlagsUpdated,
+                            SdkInternalEvent.FlagKilledNotification,
+                            SdkInternalEvent.LargeSegmentsUpdated,
+                            SdkInternalEvent.SegmentsUpdated
+                    }
+                },
+                { SdkEvent.SdkReadyTimeout, new HashSet<SdkInternalEvent> 
+                    {
+                            SdkInternalEvent.SdkTimedOut
+                    }
+                }
+            };
+            Dictionary<SdkEvent, HashSet<SdkInternalEvent>> suppressedBy = new Dictionary<SdkEvent, HashSet<SdkInternalEvent>>
+            {
+                { SdkEvent.SdkReadyTimeout, new HashSet<SdkInternalEvent>
+                    { SdkInternalEvent.SdkReady }
+
+                }
+            };
+            Dictionary<SdkEvent, int> executionLimits = new Dictionary<SdkEvent, int>
+            {
+                { SdkEvent.SdkReadyTimeout, 1 },
+                { SdkEvent.SdkUpdate, -1 }
+            };
+
+            return new EventsManagerConfig(requireAll, requireAny, prerequisites, suppressedBy, executionLimits);
+        }
+    }
+}
---
+++ tests/Splitio-tests/Unit Tests/Common/EventsManagerConfigTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Splitio.Domain;
+
+namespace Splitio_Tests.Unit_Tests.Common
+{
+    [TestClass]
+    public class EventsManagerConfigTests
+    {
+
+        [TestMethod]
+        public void BuildInstance()
+        {
+            //Act
+            EventsManagerConfig config = EventsManagerConfig.BuildEventsManagerConfig();
+
+            //Assert
+            config.ExecutionLimits.TryGetValue(SdkEvent.SdkReadyTimeout, out var timout);
+            Assert.AreEqual(1, timout);
+            config.ExecutionLimits.TryGetValue(SdkEvent.SdkUpdate, out var update);
+            Assert.AreEqual(-1, update);
+
+            config.RequireAny.TryGetValue(SdkEvent.SdkReadyTimeout, out var require1);
+            Assert.AreEqual(1, require1.Count);
+            Assert.IsTrue(require1.Contains(SdkInternalEvent.SdkTimedOut));
+            config.RequireAny.TryGetValue(SdkEvent.SdkUpdate, out var require2);
+            Assert.AreEqual(5, require2.Count);
+            Assert.IsTrue(require2.Contains(SdkInternalEvent.SegmentsUpdated));
+            Assert.IsTrue(require2.Contains(SdkInternalEvent.LargeSegmentsUpdated));
+            Assert.IsTrue(require2.Contains(SdkInternalEvent.RuleBasedSegmentsUpdated));
+            Assert.IsTrue(require2.Contains(SdkInternalEvent.FlagKilledNotification));
+            Assert.IsTrue(require2.Contains(SdkInternalEvent.FlagsUpdated));
+        }
+    }
+}
