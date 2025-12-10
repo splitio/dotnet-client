e646226528de2779c9abbcc7a176def334d64c2a
Bill Al <bilal.al-shahwany@harness.io>
2025-12-09 21:10:30 -0800

    added event manager

--- src/Splitio/Services/Common/IEventDelivery.cs
+++
@@ -1,9 +0,0 @@
-﻿using Splitio.Domain;
-
-namespace Splitio.Services.Common
-{
-    public interface IEventDelivery
-    {
-        void Deliver(SdkEvent sdkEvent, EventMetadata eventMetadata);
-    }
-}
--- src/Splitio/Services/Common/IEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using Splitio.Domain;
-
-namespace Splitio.Services.Common
-{
-    public interface IEventHandler
-    {
-        void Handle(SdkEvent sdkEvent, EventMetadata eventMetadata);
-    }
-}
